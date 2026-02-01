83342944c78951ba148eb01b434692410231b58c
Silent Demon SD ( MysterySD ) <105407900+SilentDemonSD@users.noreply.github.com>
2026-02-01 20:53:57 +0530

    refactor: code changes

--- send_mail.py
+++ send_mail.py
@@ -3,33 +3,19 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 SENDER = "cirilabangg@gmail.com"
-RECEIVER = "mysterysd.sd@gmail.com"
+RECEIVER = "mysterysd.sd@gmail.com" #kumarhimanshu1104k@gmail.com
 APP_PASSWORD = "sptu fmem shui oblr"
 
 def read(path):
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
 
-base = read("base.html")
-header = read("blocks/header.html")
-divider = read("blocks/divider.html")
-content = read("blocks/content.html")
-footer = read("blocks/footer.html")
-
-content = (
-    content
-    .replace("{{FNAME}}", "John")
-    .replace("{{DATE}}", "2026/01/30")
-)
+final_html = read("base.html")
 
 final_html = (
-    base
-    .replace("{{HEADER}}", header)
-    .replace("{{DIVIDER}}", divider)
-    .replace("{{CONTENT}}", content)
-    .replace("{{FOOTER}}", footer)
+    final_html
+    .replace("{{FNAME}}", "Sp/Team")
 )
-
 
 msg = MIMEMultipart("alternative")
 msg["Subject"] = "Community Partnership Proposal"
